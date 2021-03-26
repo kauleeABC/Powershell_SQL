e431da5d237dc55ff650b61ece34f2ac00b6207b
kauleeABC <81389614+kauleeABC@users.noreply.github.com>
2021-03-26 09:36:34 -0500

    Add files via upload

---
+++ get_SQL_Services_Multiple_Servers.ps1
@@ -0,0 +1,22 @@
+﻿<#
+create by: Kau Lee
+get SQL server services from multiple servers
+this can be automate with txt file with list of servers or services
+by 
+
+$servers = Get-Content .\Serverlist.txt
+$services = Get-Content .\Serviceslist.txt
+#>
+
+$servers= 't79tdw171sdb003', 't79tdw171sdb013'
+$services='MSSQLSERVER', 'SQLSERVERAGENT','MSSQLServerOLAPService'
+##$services
+
+foreach ($s in $servers)
+{
+    foreach ($sv in $services)
+    {
+        Get-WmiObject -Query "select * from win32_service where name = '$sv'" -ComputerName $s |
+        Format-List -Property PSComputerName, Name, ExitCode, Name, ProcessID, StartMode, State, Status
+    }
+}
