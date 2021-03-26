08e85a87ba4662b90f8b8c2de585453f16a031df
kauleeABC <81389614+kauleeABC@users.noreply.github.com>
2021-03-26 10:11:29 -0500

    Add files via upload

---
+++ SQL_services_All.ps1
@@ -0,0 +1,21 @@
+﻿<#
+create by: Kau Lee
+get SQL server services log on as account from multiple servers
+this can be automate with txt file with list of servers or services
+by 
+$servers = Get-Content .\Serverlist.txt
+$services = Get-Content .\Serviceslist.txt
+#> 
+
+#replace your server's name
+$servers= 'server1' , 'server2'
+$services='MSSQLSERVER', 'SQLSERVERAGENT','MSSQLServerOLAPService'
+ ## (Get-WmiObject Win32_Service -Filter "Name='$serviceName'" -ComputerName T79TDW171SDB013).StartName 
+foreach ($s in $servers)
+{
+    foreach ($sv in $services)
+    {
+        Get-CimInstance -ClassName CIM_Service  -ComputerName $s -Filter "Name='$sv'"| 
+        Select-Object PSComputerName, Name, StartName, state, StartMode, status
+    }
+}
