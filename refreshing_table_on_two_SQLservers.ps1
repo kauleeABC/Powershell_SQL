0947f22943a72a4341c4332344edcb016db57049
kauleeABC <81389614+kauleeABC@users.noreply.github.com>
2021-04-01 13:06:10 -0500

    Add files via upload

---
+++ refreshing_table_on_two_SQLservers.ps1
@@ -0,0 +1,24 @@
+﻿<#
+author: Kau Lee
+Date:4/1/2021
+#>
+
+#SQL server and query
+$server="source_server_name"
+$server2="Destination_server_name"
+$database="DB_name"
+$tableName="table_name"
+$query="SELECT top 15 * FROM sourcetable" #,"SELECT top 10 * FROM Pegasys.dbo.XACTION" 
+#$queryValicate=" SELECT COUNT(*)  FROM Test_DB.dbo.xaction"
+$query_trucate="TRUNCATE TABLE destination_table"
+
+#stored data set on $data veriable 
+     
+        #truncate destination table before insert now data
+          Invoke-Sqlcmd -Query "$query_trucate" -ServerInstance "$server2"
+
+          #query data from source table
+          Invoke-Sqlcmd -Query "$query" -ServerInstance "$server" | #|  Export-Csv -Path "$filename.csv" -NoTypeInformation
+           
+        #write data to destination table
+          Write-SQLTableData -ServerInstance $server2 -DatabaseName $database -SchemaName "dbo" -TableName $tableName
