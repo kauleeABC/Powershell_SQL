11c557270e8b337d14569a25f0637526d3a68b26
kauleeABC <81389614+kauleeABC@users.noreply.github.com>
2022-01-04 13:53:00 -0600

    Add files via upload

---
+++ migrateSQLDBTemplate.ps1
@@ -0,0 +1,2 @@
+﻿#using DBATOOLS to migrate database to new server 
+Copy-DbaDatabase -Source "ServerName" -Destination "ServerName" -Database "DBName" -BackupRestore -SharedPath "shared file location"
